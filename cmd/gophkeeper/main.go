abdfd0730844759cadd189dc39a63797dd46c0b6
iwankho <43066471+xoxloviwan@users.noreply.github.com>
2024-12-19 23:43:54 +0300

    add version info for cli

--- cmd/gophkeeper/main.go
+++ cmd/gophkeeper/main.go
@@ -1,12 +1,32 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"iwakho/gopherkeep/internal/cli/views"
+	"log"
 	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
 )
+
+var (
+	buildVersion = "N/A"
+	buildDate    = "N/A"
+	buildCommit  = "N/A"
+	vers         = flag.Bool("v", false, "version")
+)
+
+func init() {
+	flag.Parse()
+	if len(flag.Args()) > 0 {
+		log.Fatal("Too many arguments")
+	}
+	if *vers {
+		fmt.Printf("Build version: %s\nBuild date: %s\nBuild commit: %s\n", buildVersion, buildDate, buildCommit)
+		os.Exit(0)
+	}
+}
 
 func main() {
 	m, err := views.NewSampleConfigUI()
