c45e43b24f3da43e7a9cc141c7fd82fa735a595d
iwankho <43066471+xoxloviwan@users.noreply.github.com>
2024-12-20 02:09:14 +0300

    basic auth draft

--- internal/srv/http/middleware/access.go
+++ internal/srv/http/middleware/access.go
@@ -1,10 +1,53 @@
 package middleware
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
-func CheckAuth(h http.Handler) http.Handler {
+func CheckAuth(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		// auth := r.Header.Get("Authorization")
-		h.ServeHTTP(w, r)
+		// Extract the username and password from the request
+		// Authorization header. If no Authentication header is present
+		// or the header value is invalid, then the 'ok' return value
+		// will be false.
+		username, password, ok := r.BasicAuth()
+		if ok {
+			fmt.Println(username, password)
+			// TODO: Добавить проверку логина и пароля в базе данных
+			next.ServeHTTP(w, r)
+			return
+			// // Calculate SHA-256 hashes for the provided and expected
+			// // usernames and passwords.
+			// usernameHash := sha256.Sum256([]byte(username))
+			// passwordHash := sha256.Sum256([]byte(password))
+			// expectedUsernameHash := sha256.Sum256([]byte("your expected username"))
+			// expectedPasswordHash := sha256.Sum256([]byte("your expected password"))
+
+			// // Use the subtle.ConstantTimeCompare() function to check if
+			// // the provided username and password hashes equal the
+			// // expected username and password hashes. ConstantTimeCompare
+			// // will return 1 if the values are equal, or 0 otherwise.
+			// // Importantly, we should to do the work to evaluate both the
+			// // username and password before checking the return values to
+			// // avoid leaking information.
+			// usernameMatch := (subtle.ConstantTimeCompare(usernameHash[:], expectedUsernameHash[:]) == 1)
+			// passwordMatch := (subtle.ConstantTimeCompare(passwordHash[:], expectedPasswordHash[:]) == 1)
+
+			// // If the username and password are correct, then call
+			// // the next handler in the chain. Make sure to return
+			// // afterwards, so that none of the code below is run.
+			// if usernameMatch && passwordMatch {
+			// 	next.ServeHTTP(w, r)
+			// 	return
+			// }
+		}
+
+		// If the Authentication header is not present, is invalid, or the
+		// username or password is wrong, then set a WWW-Authenticate
+		// header to inform the client that we expect them to use basic
+		// authentication and send a 401 Unauthorized response.
+		w.Header().Set("WWW-Authenticate", `Basic realm="restricted", charset="UTF-8"`)
+		http.Error(w, "Unauthorized", http.StatusUnauthorized)
 	})
 }
