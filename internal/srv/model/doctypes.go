481ee06bf94520bb757e89be3425421f130b3300
Иван Хохлов <43066471+xoxloviwan@users.noreply.github.com>
2024-12-23 09:03:52 +0300

    change structure

--- internal/srv/model/doctypes.go
+++
@@ -1,8 +0,0 @@
-package model
-
-const (
-	ItemTypeLoginPass = iota
-	ItemTypeText
-	ItemTypeBinary
-	ItemTypeCard
-)
--- internal/srv/model/passport.go
+++ internal/srv/model/passport.go
@@ -2,6 +2,13 @@ package model
 
 import "time"
 
+const (
+	ItemTypeLoginPass = iota
+	ItemTypeText
+	ItemTypeBinary
+	ItemTypeCard
+)
+
 type Metainfo struct {
 	Date time.Time `json:"creationDate"`
 	Text string    `json:"info"`
