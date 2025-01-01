a2fbec544055114b2b0241bc544f884a6137c54f
Иван Хохлов <43066471+xoxloviwan@users.noreply.github.com>
2025-01-01 17:05:17 +0300

    list description

--- internal/cli/views/list/item.go
+++ internal/cli/views/list/item.go
@@ -29,6 +29,10 @@ func (d itemDelegate) Render(w io.Writer, m list.Model, index int, listItem list
 
 	str := fmt.Sprintf("%d. %s", index+1, i.Title)
 
+	if i.Description != "" {
+		str = str + fmt.Sprintf("\n %s", i.Description)
+	}
+
 	fn := itemStyle.Render
 	if index == m.Index() {
 		fn = func(s ...string) string {
