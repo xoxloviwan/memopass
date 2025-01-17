beb2c35aa3c39884d5ccc70522256105d3514187
iwankho <43066471+xoxloviwan@users.noreply.github.com>
2025-01-17 20:21:28 +0300

    add tests

---
+++ internal/cli/views/items/textarea/add/textarea_test.go
@@ -0,0 +1,32 @@
+package textarea
+
+import (
+	"testing"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/knz/catwalk"
+)
+
+type MockController struct{}
+
+func (c *MockController) AddText(string) error {
+	return nil
+}
+
+type TextAreaPageWrapper struct {
+	*TextareaPage
+}
+
+func (m *TextAreaPageWrapper) Init() tea.Cmd {
+	return nil
+}
+
+func (m *TextAreaPageWrapper) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	return m.TextareaPage.Update(m, msg)
+}
+
+func TestAuth(t *testing.T) {
+	// Initialize the model to test.
+	m := NewPage(func() {}, &MockController{})
+	catwalk.RunModel(t, "textarea", &TextAreaPageWrapper{m})
+}
