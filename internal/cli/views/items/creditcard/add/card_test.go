ccc129d49700089ee119a239adcee151708446bb
iwankho <43066471+xoxloviwan@users.noreply.github.com>
2025-01-17 19:25:08 +0300

    add test for card view

---
+++ internal/cli/views/items/creditcard/add/card_test.go
@@ -0,0 +1,33 @@
+package creditcard
+
+import (
+	"iwakho/gopherkeep/internal/model"
+	"testing"
+
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/knz/catwalk"
+)
+
+type MockController struct{}
+
+func (c *MockController) AddCard(p model.Card) error {
+	return nil
+}
+
+type CardPageWrapper struct {
+	*CardPage
+}
+
+func (m *CardPageWrapper) Init() tea.Cmd {
+	return nil
+}
+
+func (m *CardPageWrapper) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	return m.CardPage.Update(m, msg)
+}
+
+func TestAuth(t *testing.T) {
+	// Initialize the model to test.
+	m := NewPage(func() {}, &MockController{})
+	catwalk.RunModel(t, "card", &CardPageWrapper{m})
+}
