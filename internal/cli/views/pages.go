b0261c7d53e313f56a4159cfd3fc5e4b7a9b8b28
Иван Хохлов <43066471+xoxloviwan@users.noreply.github.com>
2025-01-19 16:38:05 +0300

    draft embed pager

--- internal/cli/views/pages.go
+++ internal/cli/views/pages.go
@@ -7,6 +7,7 @@ import (
 	addPair "iwakho/gopherkeep/internal/cli/views/items/pair/add"
 	showPairs "iwakho/gopherkeep/internal/cli/views/items/pair/show"
 	addText "iwakho/gopherkeep/internal/cli/views/items/textarea/add"
+	showTexts "iwakho/gopherkeep/internal/cli/views/items/textarea/show"
 	"iwakho/gopherkeep/internal/cli/views/login"
 	"iwakho/gopherkeep/internal/cli/views/menu"
 
@@ -31,6 +32,8 @@ type Pages struct {
 	pages       []Page
 	currentPage int
 	Sender
+	width  int
+	height int
 }
 
 type Controller interface {
@@ -43,12 +46,13 @@ type Controller interface {
 	addText.Control
 }
 
+const offset = 2
+
 func InitPages(ctrl Controller) *Pages {
 	const pageTotal = 10
 	p := Pages{
 		pages: make([]Page, pageTotal),
 	}
-	const offset = 2
 
 	p.add(0, login.NewPage(p.nextPage(1), ctrl))
 	p.add(1, menu.NewPage(func(id int) {
@@ -62,7 +66,7 @@ func InitPages(ctrl Controller) *Pages {
 	p.add(offset+0, addPair.NewPage(p.nextPage(1), ctrl))
 	p.add(offset+1, showPairs.NewPage(p.nextPage(1), ctrl))
 	p.add(offset+2, addText.NewPage(p.nextPage(1), ctrl))
-	p.add(offset+3, p.get(1)) // TODO text viewer
+	p.add(offset+3, showTexts.NewPage(p.nextPage(1)))
 	p.add(offset+4, picker.NewPage(p.nextPage(1), ctrl))
 	p.add(offset+5, p.get(1)) // TODO file download
 	p.add(offset+6, addCard.NewPage(p.nextPage(1), ctrl))
@@ -96,8 +100,10 @@ func (*Pages) Init() tea.Cmd {
 }
 
 func (s *Pages) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-	switch msg.(type) {
+	switch msg := msg.(type) {
 	case tea.WindowSizeMsg:
+		s.width = msg.Width
+		s.height = msg.Height
 		if !ready {
 			ready = true
 			p := s.pages[s.currentPage]
@@ -111,9 +117,11 @@ func (s *Pages) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 	if s.currentPage != lastPage {
 		cmd = tea.Batch(cmd, s.pages[s.currentPage].Init())
-
+		if s.currentPage == offset+3 {
+			_, cmd1 := s.pages[s.currentPage].Update(tea.WindowSizeMsg{Width: s.width, Height: s.height})
+			cmd = tea.Batch(cmd, cmd1)
+		}
 	}
-	// fmt.Println(s.currentPage, "Pages")
 	return s, cmd
 }
 
